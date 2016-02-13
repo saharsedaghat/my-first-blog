10354d5a03a603b9d969b858b46c84c112c8a194
saharsedaghat <s.sedaghat1004@gmail.com>
2016-02-13 14:33:14 +0000

    ffs

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+	url (r'^admin/', include(admin.site.urls)),
+	url (r'', include('blog.urls')),
+	]
